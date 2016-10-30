5abee38d35e661352ee5ae97d17bc5d2f10e4d7b
junming4 <2284876299@qq.com>
2016-10-30 17:09:19 +0800

    'edit'

---
+++ blog/class/Mysql.class.py
@@ -0,0 +1,8 @@
+class Mysql(object):
+    #
+    def __init__(self, name, age, weight):
+        self._name = name
+        self._age = age
+        self._weight = weight
+
+
--- blog/tests.py
+++ blog/tests.py
@@ -8,3 +8,13 @@ from django.test import TestCase
 
 def test(request):
     return HttpResponse('<html>hello django</html')
+
+import MySQLdb
+
+MySQLdb.connect(
+    host = '127.0.0.1',
+    port = 3306,
+    user = 'root',
+    db = 'pythontest_db',
+    charset = 'utf8'
+)
